d8669875c56c1e56cff5c11c371f44f7ce3dfd6e
陈小东 <xiaodong.chen@mtime.com>
2018-09-11 19:39:46 +0800

    1. add unit test for replacerule 2. add go.mod to image

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY . /go/src/goproxy
 RUN cd /go/src/goproxy &&\
     CGO_ENABLED=0 GO111MODULE=on go build -o /app/goproxy
 
+COPY go.mod /app
+
 # step: run
 FROM golang:1.11-alpine3.8
 LABEL maintainer="dongdongking008 <dongdongking008@gmail.com>"
---
+++ replacerule/replacerule_test.go
@@ -0,0 +1,25 @@
+package replacerule
+
+import (
+	"testing"
+)
+
+func TestParseOneRule(t *testing.T) {
+	manager := GetManager("mtime.com/(.*) gitlab.mtime-dev.com/go/$1.git")
+	path := manager.Replace("mtime.com/demo")
+	if path != "gitlab.mtime-dev.com/go/demo.git" {
+		t.Errorf("error path: %s", path)
+	} else {
+		t.Logf("path: %s", path)
+	}
+}
+
+func TestParseTwoRule(t *testing.T) {
+	manager := GetManager("mtime.com/(.*) gitlab.mtime-dev.com/go/$1.git,mtime.com/core/(.*) gitlab.mtime-dev.com/go/core/$1.git;")
+	path := manager.Replace("mtime.com/demo")
+	if path != "gitlab.mtime-dev.com/go/demo.git" {
+		t.Errorf("error path: %s", path)
+	} else {
+		t.Logf("path: %s", path)
+	}
+}
